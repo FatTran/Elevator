267cf7d0bf56144322dda2581752e1fbba1ef0d9
FatTran <123949287+FatTran@users.noreply.github.com>
2023-04-22 21:28:52 +0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,5 +1,5 @@
-void up_queue(int floor, int floor_queue[], int n);
-void down_queue( int floor, int floor_queue[], int n);
-void delete_queue(int floor_queue[], int n);
-int checker(int value, int floor_queue[], int n);
+void up_queue(int floor, int floor_queue[]);
+void down_queue( int floor, int floor_queue[]);
+void delete_queue(int floor_queue[]);
+int checker(int floor, int floor_queue[]);
 void add_queue(int floor, int cur_floor, int up_floor_queue[], int down_floor_queue[]);
